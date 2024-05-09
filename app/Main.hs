30dfdc01e92cfc587be2f8d2d3b956f16218fa15
Vismay <vraj@conviva.com>
2024-05-09 14:19:10 +0530

    added option to read from commandline

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,21 @@ module Main (main) where
 import Parser.Parser (parseFile)
 import Graph.Graph (createDotOutput)
 import Data.ByteString.Char8 (unpack)
+import System.Environment (getArgs)
 
 main :: IO ()
-main = do 
-  graph <- parseFile "data/test.dot"
-  -- print graph
-  case graph of 
-    Left _ -> return ()
-    Right g -> putStrLn $ unpack $ createDotOutput g
+main = do
+  args <- getArgs
+  case args of
+    filename:_ -> do
+      graph <- parseFile filename
+      case graph of 
+        Left _ -> return ()
+        Right g -> putStrLn $ unpack $ createDotOutput g
+    [] -> do
+        print "no arguments provided"
+  -- graph <- parseFile "data/test.dot"
+  -- -- print graph
+  -- case graph of 
+  --   Left _ -> return ()
+  --   Right g -> putStrLn $ unpack $ createDotOutput g
